0daa8c453c1bb8fac7410cc41fc93ce4136f03f4
Ivan Smirnov <i.s.smirnov@gmail.com>
2016-02-07 21:16:52 +0000

    Initial commit: node/gulp/webpack/eslint/babel configuration + node build

---
+++ gulp/webpack.config.babel.js
@@ -0,0 +1,104 @@
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import path from 'path';
+import webpack from 'webpack';
+
+function pathTo(relpath) {
+    return path.join(__dirname, '..', relpath);
+}
+
+let config = {
+    entry: {
+        'app': pathTo('js/app'),
+        vendor: [
+            'react', 'sockjs-client', 'underscore', 'debug', 'alt', 'immutable',
+            'whatwg-fetch', 'material-ui', 'react-tap-event-plugin', 'rc-slider',
+            'react-flexbox-grid'
+        ]
+    },
+    output: {
+        path: pathTo('static'),
+        publicPath: '/static/',
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(css|scss)$/,
+                loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap')
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: [
+                        'es2015-webpack',
+                        'react'
+                    ],
+                    plugins: [
+                        'transform-decorators-legacy',
+                        'transform-class-properties',
+                        'transform-object-rest-spread'
+                    ],
+                    cacheDirectory: true
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.json', '.scss'],
+        modulesDirectories: ['node_modules']
+    },
+    node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty',
+        process: 'empty'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            'root.jQuery': 'jquery',
+            React: 'react',
+            _: 'underscore',
+            'es6-promise': 'es6-promise',
+            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'vendor.bundle.js'
+        }),
+        new ExtractTextPlugin('../static/vendor.css', { allChunks: true })
+    ],
+    stats: {
+        colors: true
+    }
+};
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMaps: false,
+            minimize: true,
+            compress: {
+                warnings: false
+            },
+            output: {
+                comments: false,
+                semicolons: true
+            }
+        })
+    );
+} else {
+    config.devtool = 'eval-cheap-module-source-map';
+}
+
+export default config;
---
+++ gulpfile.babel.js
@@ -0,0 +1,36 @@
+import gulp from 'gulp';
+import googleWebfonts from 'gulp-google-webfonts';
+import gulpUtil from 'gulp-util';
+import path from 'path';
+import webpack from 'webpack';
+import webpackConfig from './gulp/webpack.config.babel';
+
+function pathTo(relpath) {
+    return path.join(__dirname, relpath);
+}
+
+gulp.task('fonts', () =>
+    gulp.src('gulp/fonts.list')
+        .pipe(googleWebfonts({}))
+        .pipe(gulp.dest(pathTo('static/fonts')))
+);
+
+gulp.task('webpack', callback => {
+    function onError(error, stats) {
+        if (error) {
+            return callback(new gulpUtil.PluginError('webpack', error));
+        } else {
+            gulpUtil.log('[webpack]', stats.toString());
+            return callback();
+        }
+    }
+    webpack(webpackConfig, onError);
+});
+
+gulp.task('build', ['fonts', 'webpack']);
+
+gulp.task('watch', ['webpack'], () => {
+    gulp.watch(['js/**/*.js'], ['webpack'])
+});
+
+gulp.task('default', ['build']);
