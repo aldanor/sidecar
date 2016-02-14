b81398a6d1b03fa35c46c3dfdbab60b7a986231f
Ivan Smirnov <i.s.smirnov@gmail.com>
2016-02-14 20:07:47 +0000

    Complete tags tests

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import keyword
+import re
 import pytest
 
 from sidecar import tags
 from sidecar.element import Element, expr
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def all_tags():
     return [{
         'name': tag,
@@ -17,17 +18,13 @@ def all_tags():
 
 
 class TestTags(object):
-    def test_basic(self, all_tags):
+    def test_tags(self, all_tags):
         for tag in all_tags:
             assert issubclass(tag['cls'], Element)
             element = tag['cls'](
                 is_='foo', form_enc_type='bar', alt='baz', inputMode='qwe', href=expr('x'),
                 style={'line-height': 1, 'align_self': 'no', 'flexFlow': 'yes', 'flex': 0}
             )
-            assert element.name == tag['name']
-            assert element.is_container == (not tag['void'])
-            assert element.inputs == []
-            assert element.outputs == []
             assert element.to_dict() == {
                 '__element__': {
                     'name': tag['name'],
@@ -49,6 +46,12 @@ class TestTags(object):
                     }
                 }
             }
+            assert element.name == tag['name']
+            assert element.is_container == (not tag['void'])
+            assert element.inputs == []
+            assert element.outputs == []
+            assert element.children == []
+            assert element.props['href'] == expr('x')
 
     def test_void_tags(self, all_tags):
         for tag in all_tags:
@@ -59,3 +62,38 @@ class TestTags(object):
                     tag['cls']()['foo']
             else:
                 assert tag['cls']['foo', 'bar'] == tag['cls']()['foo', 'bar']
+
+    def test_attributes(self):
+        for attr in tags._ATTRIBUTES:
+            if attr == 'style':
+                continue
+            py_attr = attr + '_' * keyword.iskeyword(attr)
+            js_attr = re.sub(r'_([a-z])', lambda s: s.group(1).upper(), attr)
+            element = tags.p(**{py_attr: 'foo'})['baz']
+            assert element.to_dict() == {
+                '__element__': {'name': 'p', 'children': ['baz'], 'props': {js_attr: 'foo'}}
+            }
+            assert element == tags.p['baz'](**{py_attr: 'foo'})
+        assert tags.p(is_=expr('z')).props['is'] == expr('z')
+
+    def test_invalid_attribute(self):
+        pytest.raises_str('unknown attribute: bar', tags.p, bar='foo')
+        pytest.raises_str('unknown attribute: bar', tags.p['baz'], bar='foo')
+        pytest.raises_str('invalid attribute: href=42', tags.p, href=42)
+
+    def test_styles(self):
+        for style in tags._STYLES:
+            element = tags.p(style={style: 'foo'})
+            js_style = re.sub(r'[\-_]([a-z])', lambda s: s.group(1).upper(), style)
+            assert element.to_dict() == {
+                '__element__': {'name': 'p', 'children': [], 'props': {'style': {js_style: 'foo'}}}
+            }
+        assert tags.p(style={'z-index': 1.2}).props['style']['zIndex'] == 1.2
+        assert tags.p(style={'z_index': 42}).props['style']['zIndex'] == 42
+        assert tags.p(style={'zIndex': expr('y')}).props['style']['zIndex'] == expr('y')
+        assert tags.p(style={'filter': 'foo'}).props['style']['filter'] == 'foo'
+
+    def test_invalid_style(self):
+        pytest.raises_str('invalid style: []', tags.p, style=[])
+        pytest.raises_str('unknown style: foo', tags.p, style={'foo': 1})
+        pytest.raises_str('invalid style: zIndex=[]', tags.p, style={'z-index': []})
