d66f480222543ac40c1315a4ab6d8c920bfd43ae
Ivan Smirnov <i.s.smirnov@gmail.com>
2016-02-20 02:09:13 +0000

    Update setup.py, add MANIFEST.in

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setup(
     version=version,
     author='Ivan Smirnov',
     author_email='i.s.smirnov@gmail.com',
+    license='Apache',
+    url='https://github.com/aldanor/sidecar',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
@@ -20,13 +22,24 @@ setup(
         'tornado',
         'sockjs-tornado'
     ],
+    include_package_data=True,
+    package_data={'': [
+        'static/*.css',
+        'static/*.js',
+        'static/*.html',
+        'static/fonts/*.css',
+        'static/fonts/*.woff'
+    ]},
     classifiers=[
         'Development Status :: 1 - Planning',
         'Environment :: Web Environment',
         'Intented Audience :: Developers',
         'Framework :: Tornado',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: JavaScript',
         'Operating System :: OS Independent',
         'Topic :: WWW/HTTP :: Dynamic Content'
     ]
